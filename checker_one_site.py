c50ba0c70a48f179a535f442bd100a4da9a0df5d
Ray2711 <nurassyl.tagayev@gmail.com>
2025-06-19 02:17:20 +0500

    added checker for one website

---
+++ checker_one_site.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+import time
+
+
+def main():
+
+    while True:
+        os.system('python vfs_fra_min.py')
+        time.sleep(240)
+
+if __name__ == '__main__':
+    main()
