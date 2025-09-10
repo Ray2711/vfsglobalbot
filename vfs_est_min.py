697ac138549c146907ff43248659aea8f5719324
Ray2711 <nurassyl.tagayev@gmail.com>
2025-09-10 13:58:41 +0500

    added estonia finland norway

---
+++ vfs_est_min.py
@@ -0,0 +1,4 @@
+  
+from vfs_main import vfs_checkdates
+
+vfs_checkdates("https://visa.vfsglobal.com/kaz/en/est/login","Almaty","Astana","Эст Алм","Эст Аст",False, False)
---
+++ vfs_fin_min.py
@@ -0,0 +1,4 @@
+  
+from vfs_main import vfs_checkdates
+
+vfs_checkdates("https://visa.vfsglobal.com/kaz/en/fin/login","Almaty","Astana","Фин Алм","Фин Аст",False, False)
